c15b1cfbcf317c552f274a015f23ad2687c1c899
Joey Conroy <come2thecabaret@gmail.com>
2016-06-03 12:46:09 -0400

    assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@ posts = Post.all
   )
 end
 
+Post.find_or_create_by(title: "Unique Title", body: "Unique body")
+puts posts.count
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
