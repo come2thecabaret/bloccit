bf4be4cf9d1c09de448853db92add5cb246d6d71
Joey Conroy <come2thecabaret@gmail.com>
2016-06-21 21:58:07 -0400

    assignment 36 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.name = name.split(" ").each.downcase.capitalize.join(' ') }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
