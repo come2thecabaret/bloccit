bd5e6956cbcf97da262e13ec37b5fa1de7bc0fb5
Joey Conroy <come2thecabaret@gmail.com>
2016-06-29 15:29:17 -0400

    checkpoint 39 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 require 'random_data'
+5.times do
+  User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -9,6 +19,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+  user: users.sample,
   topic: topics.sample,
   title: RandomData.random_sentence,
   body: RandomData.random_paragraph
@@ -23,7 +34,14 @@ posts = Post.all
   )
 end
 
+ user = User.first
+ user.update_attributes!(
+   email: 'come2thecabaret@gmail.com', # replace this with your personal email
+   password: 'helloworld'
+ )
+ 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
