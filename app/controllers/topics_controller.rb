92ce561ed9ca9ae10c78efbfea98c8b336cfed1f
Joey Conroy <come2thecabaret@gmail.com>
2016-06-30 16:04:42 -0400

    assignment 40 completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
 
-  before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_moderator, only: [:edit, :update]
+  before_action :authorize_admin, except: [:index, :show, :update, :edit]
   def index
     @topics = Topic.all
   end
@@ -61,10 +61,17 @@ end
     params.require(:topic).permit(:name, :description, :public)
   end
 
-  def authorize_user
-    unless current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
+  def authorize_moderator
+    unless current_user.moderator? || current_user.admin?
+      flash[:error] = 'You must be an admin or moderator to do that.'
       redirect_to topics_path
     end
   end
+
+  def authorize_admin
+    unless current_user.admin?
+      flash[:error] = 'You must be an admin to do that.'
+      redirect_to topics_path
+    end
+end
 end
--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,8 @@
 module TopicsHelper
   def user_is_authorized_for_topics?
-     current_user && current_user.admin?
+     current_user &&  (current_user.admin? || current_user.moderator?)
+  end
+  def user_is_authorized_for_topics_delete_or_create?
+    current_user && current_user.admin?
   end
 end
