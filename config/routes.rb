61d3c8fd5d9592a608593fbe34c2f06642989a24
Joey Conroy <come2thecabaret@gmail.com>
2016-07-09 18:36:31 -0400

    checkpoint 44 completed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ resources :labels, only: [:show]
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
     resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
 
   post '/up-vote' => 'votes#up_vote', as: :up_vote
