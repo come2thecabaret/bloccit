2ebd3e18220bc769923583550323ea898ade71a2
Joey Conroy <come2thecabaret@gmail.com>
2016-07-14 15:21:27 -0400

    checkpoint 48 completed

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,11 @@ resources :labels, only: [:show]
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709202017) do
+ActiveRecord::Schema.define(version: 20160714184354) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20160709202017) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
